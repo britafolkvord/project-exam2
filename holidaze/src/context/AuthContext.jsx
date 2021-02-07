bc8bb5453672a5456665b22c23ea245982f840ba
britafolkvord <brita.folk@hotmail.com>
2021-02-07 22:26:41 +0100

    prettier format

--- holidaze/src/context/AuthContext.jsx
+++ holidaze/src/context/AuthContext.jsx
@@ -1,28 +1,27 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-    const existingUser = localStorage.getItem('username') || null
-    const existingPassword = localStorage.getItem('password') || null
+    const existingUser = localStorage.getItem('username') || null;
+    const existingPassword = localStorage.getItem('password') || null;
 
-    const [user, setUser] = useState(existingUser)
-    const [password, setPassword] = useState(existingPassword)
+    const [user, setUser] = useState(existingUser);
+    const [password, setPassword] = useState(existingPassword);
 
     function loginUser(username, userPassword) {
-
-        setUser(username)
-        setPassword(userPassword)
+        setUser(username);
+        setPassword(userPassword);
     }
 
     function logout() {
-        setUser(null)
-        setPassword(null)
-        localStorage.removeItem('username')
-        localStorage.removeItem('password')
+        setUser(null);
+        setPassword(null);
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
     }
 
-    return <AuthContext.Provider value={{ user, password, loginUser, logout }}>{children}</AuthContext.Provider>
-}
+    return <AuthContext.Provider value={{ user, password, loginUser, logout }}>{children}</AuthContext.Provider>;
+};
 
-export { AuthContext, AuthContextProvider }
+export { AuthContext, AuthContextProvider };
