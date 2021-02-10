ac9b47f22fa38dbb314cb5820530cfdb528a472b
britafolkvord <brita.folk@hotmail.com>
2021-02-10 18:05:06 +0100

    bootstrap icons import

--- holidaze/src/components/accommodation/AccommodationDetails.jsx
+++ holidaze/src/components/accommodation/AccommodationDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Route, useParams, useHistory } from 'react-router-dom';
 import { Button, Container, Image, Spinner } from 'react-bootstrap';
+import { EggFried, CashStack, People } from 'react-bootstrap-icons';
 
 import { BASE_URL, headers } from '../../constants/api';
 import Heading from '../layout/Heading';
@@ -79,23 +80,15 @@ function AccommodationDetails() {
                             </div>
                             <div className={styles.info}>
                                 <div className={styles.infoGroup}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className={styles.svg}>
-                                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
-                                    </svg>
+                                    <People className={styles.icon} />
                                     <p>Maximum guests: {hotel.maxGuests}</p>
                                 </div>
                                 <div className={styles.infoGroup}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className={styles.svg}>
-                                        <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-                                        <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z" />
-                                    </svg>
+                                    <CashStack className={styles.icon} />
                                     <p>Price per night: {hotel.price}$</p>
                                 </div>
                                 <div className={styles.infoGroup}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className={styles.svg}>
-                                        <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                        <path d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
-                                    </svg>
+                                    <EggFried className={styles.icon} />
                                     <p>Self-catering: {hotel.selfCatering ? 'Yes' : 'No'}</p>
                                 </div>
                             </div>
