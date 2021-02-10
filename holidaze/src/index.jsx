7885b088fb5f40231d16b4b4ce7486ad21568b39
britafolkvord <brita.folk@hotmail.com>
2021-02-10 22:38:35 +0100

    format

--- holidaze/src/index.jsx
+++ holidaze/src/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import '../src/sass/App.scss';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+import '../src/sass/App.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
