681495252fbca87b0fde8dfefc8120d5b1dbb2cd
britafolkvord <brita.folk@hotmail.com>
2021-02-11 22:57:13 +0100

    updated links

--- holidaze/src/components/layout/Footer.jsx
+++ holidaze/src/components/layout/Footer.jsx
@@ -11,23 +11,17 @@ function Footer({ admin }) {
         <footer className={`${admin ? styles.admin : ''} ${styles.footer}`}>
             <div className={styles.col1}>
                 <p>
-                    <Link to={Routes.accommodation.accommodation} className={styles.footerLink}>
-                        Hotels
-                    </Link>
-                </p>
-                <p>
                     <Link to={Routes.contact} className={styles.footerLink}>
                         Contact
                     </Link>
                 </p>
-                <p>
-                    <Link to={Routes.login} className={styles.footerLink}>
-                        Log in
-                    </Link>
-                </p>
             </div>
             <div className={styles.col2}>
-                <p>Holidaze © 2021</p>
+                <p>
+                    <Link to={Routes.home} className={styles.footerLink}>
+                        Holidaze © 2021
+                    </Link>
+                </p>
             </div>
             <div className={styles.col3}>
                 <Facebook className={styles.icon} tabIndex={0} />
